7be278f846200218ae42e5d24961c3d68c566ce7
viktorv6741 <viktorv6741@gmail.com>
2016-03-22 20:05:59 +0200

    Update Solution_11.java

--- com/Solution_11.java
+++ com/Solution_11.java
@@ -7,6 +7,9 @@ public class Solution_11 {
     private ArrayList<String> storedStrings = new ArrayList<String>(); // Need to create an object
 
     public Solution_11() {
+        
+       // проверка длины стоки на соответсие условия
+        
         this.storedStrings.add(0, "something");    // We need to put something in 0 element
         this.breakString(storedStrings.get(0));
     }
